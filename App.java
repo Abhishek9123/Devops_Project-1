6e0edbe9740cc34299b47e62f04633e6d2b8e175
Abhishek9123 <abhishekbasavarajappa7245@gmail.com>
2022-04-06 12:54:06 +0530

    first commit

---
+++ App.java
@@ -0,0 +1,15 @@
+package Jenkins;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello Abhi!" );
+        System.out.println( "Hello Today holiday" );
+    }
+}
